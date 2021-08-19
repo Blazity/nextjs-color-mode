72b5c9253f0b5995fc11c2816e009d2e75019fb3
Bart Stefanski <contact@bstefanski.com>
2021-08-19 02:08:02 +0200

    fix example's build

--- example/pages/index.js
+++ example/pages/index.js
@@ -2,7 +2,7 @@ import { ColorModeStyles, useColorModeValue } from 'nextjs-color-mode'
 import { useCallback, useState } from 'react'
 import dynamic from 'next/dynamic'
 
-const ColorSwitcher = dynamic(() => import('./ColorSwitcher'), { ssr: false })
+const ColorSwitcher = dynamic(() => import('../components/ColorSwitcher'), { ssr: false })
 
 export default function Home() {
   const [boxColor, boxCss] = useColorModeValue('box-color', 'blue', 'red')
